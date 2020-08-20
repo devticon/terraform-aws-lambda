92481abb1e1a1eece48ddc5882c2bfc2e68619b0
krs <k.kamasa@gmail.com>
2020-08-20 16:08:36 +0200

    default runtime

--- variables.tf
+++ variables.tf
@@ -53,12 +53,12 @@ variable "handler" {
 variable "runtime" {
   description = "Lambda Function runtime"
   type        = string
-  default     = ""
+  default     = "nodejs12.x"
 
-  //  validation {
-  //    condition     = can(var.create && contains(["nodejs10.x", "nodejs12.x", "java8", "java11", "python2.7", " python3.6", "python3.7", "python3.8", "dotnetcore2.1", "dotnetcore3.1", "go1.x", "ruby2.5", "ruby2.7", "provided"], var.runtime))
-  //    error_message = "The runtime value must be one of supported by AWS Lambda."
-  //  }
+  validation {
+    condition     = can(var.create && contains(["nodejs10.x", "nodejs12.x", "java8", "java11", "python2.7", " python3.6", "python3.7", "python3.8", "dotnetcore2.1", "dotnetcore3.1", "go1.x", "ruby2.5", "ruby2.7", "provided"], var.runtime))
+    error_message = "The runtime value must be one of supported by AWS Lambda."
+  }
 }
 
 variable "lambda_role" {
